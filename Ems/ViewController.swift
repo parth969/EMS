8b555fb364907c9200ba7aff6c8d9e639e7e04d9
COE-026 <coe-026@coe-026s-imac.local>
2019-09-06 13:29:33 +0530

    Initial Commit

---
+++ Ems/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ems
+//
+//  Created by COE-026 on 06/09/19.
+//  Copyright © 2019 COE-026. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
